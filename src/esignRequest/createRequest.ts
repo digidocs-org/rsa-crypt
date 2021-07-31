abc4ffae82d989268c2554ce9324e1fd61a58171
Naman Singh <naman.icebed@gmail.com>
2021-07-31 22:00:21 +0530

    updated reference

--- src/esignRequest/createRequest.ts
+++ src/esignRequest/createRequest.ts
@@ -17,18 +17,15 @@ const createSignedXMLCallback = async (data: ISignedParam, callback: Function) =
         const X509Certificate = certificate.substring(certificate.indexOf("\n") + 1, certificate.lastIndexOf("\n") + 1).replace(/\n/g, ' ');
 
         var sig = new SignedXml()
-        sig.addReference("//*[local-name(.)='Docs']", ["http://www.w3.org/2000/09/xmldsig#enveloped-signature"])
+        sig.addReference("//*[local-name(.)='Esign']", ["http://www.w3.org/2000/09/xmldsig#enveloped-signature"], "http://www.w3.org/2000/09/xmldsig#sha1", "", "", "", true)
         //@ts-ignore
         sig.keyInfoProvider = {
             getKeyInfo: () => `<X509Data><X509SubjectName>1.2.840.113549.1.9.1=#16166e616d616e2e69636562656440676d61696c2e636f6d,CN=Digidocs Technologies,OU=ENGINEERING,O=DIGIDOCS TECHNOLOGIES PRIVATE LIMITED,L=PALAM,ST=DELHI,C=IN</X509SubjectName><X509Certificate>${X509Certificate}</X509Certificate></X509Data>`,
         }
         sig.signingKey = Buffer.from(data.key)
-        sig.canonicalizationAlgorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315"
+        sig.canonicalizationAlgorithm = "http://www.w3.org/TR/2001/REC-xml-c14n-20010315"
         sig.computeSignature(xml)
-        let jsonFile = JSON.parse(xml2json(sig.getSignedXml()))
-        jsonFile.Esign.Docs._attributes = {}
-        jsonFile.Esign.Signature.SignedInfo.Reference._attributes.URI = ""
-        let xmlFile = json2xml(jsonFile)
+        let xmlFile = sig.getSignedXml()
         return callback(null, xmlFile)
     })
 }
