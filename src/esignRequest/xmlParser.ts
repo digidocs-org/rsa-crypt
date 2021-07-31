3ff6c5151e6ad8a81caab0fd70105e70ac388dfc
Naman Singh <naman.icebed@gmail.com>
2021-07-31 23:54:09 +0530

    updated code

--- src/esignRequest/xmlParser.ts
+++ src/esignRequest/xmlParser.ts
@@ -1,5 +1,5 @@
 import convert from 'xml-js'
 
-export const xml2json = (xml: string) => convert.xml2json(xml, { compact: true, spaces: 2 });
-export const json2xml = (json: string) => convert.json2xml(json, { compact: true, spaces: 2 });
+export const xml2json = (xml: string) => convert.xml2json(xml, { compact: true, spaces: 0 });
+export const json2xml = (json: string) => convert.json2xml(json, { compact: true, spaces: 0 });
 
